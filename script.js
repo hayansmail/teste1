bdcf1838cf20d0e974c7fab2f385b533d201edb2
hayansmail <hayansmail@gmail.com>
2017-03-14 17:48:47 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -19,5 +19,3 @@ jQuery.extend(jQuery.easing,{easeInOutBack:function(e,f,a,i,h,g){if(g==undefined
 // http://javascript-source.com
 //***********************************************
 jQuery("#wowslider-container1").wowSlider({effect:"cube_over",prev:"",next:"",duration:20*100,delay:20*100,width:800,height:600,autoPlay:true,autoPlayVideo:false,playPause:true,stopOnHover:false,loop:false,bullets:1,caption:true,captionEffect:"parallax",controls:true,controlsThumb:false,responsive:1,fullScreen:false,gestures:2,onBeforeStep:0,images:0});
-script.js
-Ouvrir avec
